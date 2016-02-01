056062e1e93d74825c6baea97c317c29375f847a
Yoonsungbin <freelifestyle2550@gmail.com>
2016-02-01 20:16:55 +0900

    Dummy data test

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,4 +3,26 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('AppCtrl',['$scope','$http',function($scope,$http) {
 	console.log('Hello world controller');
+
+
+	person1 = {
+		name : 'Bin',
+		email : 'bin@gmail.com',
+		age : '26'
+	};
+
+	person2 = {
+		name : 'David',
+		email : 'David@gmail.com',
+		age : '26'
+	};
+
+	person3 = {
+		name : 'Sam',
+		email : 'Sam@gmail.com',
+		age : '26'
+	};
+
+	var personlist = [person1,person2,person3];
+	$scope.personlist = personlist;
 }]);
