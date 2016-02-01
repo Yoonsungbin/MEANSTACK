7e667352f135190cd577868c237d0dc69635b25b
Yoonsungbin <freelifestyle2550@gmail.com>
2016-02-01 20:27:12 +0900

    Data send

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,26 +4,8 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',function($scope,$http) {
 	console.log('Hello world controller');
 
-	$http.get('/personlist')
-
-	person1 = {
-		name : 'Bin',
-		email : 'bin@gmail.com',
-		age : '26'
-	};
-
-	person2 = {
-		name : 'David',
-		email : 'David@gmail.com',
-		age : '26'
-	};
-
-	person3 = {
-		name : 'Sam',
-		email : 'Sam@gmail.com',
-		age : '26'
-	};
-
-	var personlist = [person1,person2,person3];
-	$scope.personlist = personlist;
+	$http.get('/personlist').success(function (response) {
+		console.log('Got data from request');
+		$scope.personlist = response;
+	})
 }]);
--- server.js
+++ server.js
@@ -5,6 +5,28 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/personlist',function ( req , res ){
 	console.log("Get request from personlist");
+
+
+	person1 = {
+		name : 'Bin',
+		email : 'bin@gmail.com',
+		age : '26'
+	};
+
+	person2 = {
+		name : 'David',
+		email : 'David@gmail.com',
+		age : '26'
+	};
+
+	person3 = {
+		name : 'Sam',
+		email : 'Sam@gmail.com',
+		age : '26'
+	};
+
+	var personlist = [person1,person2,person3];
+	res.json(personlist);
 });
 
 app.listen(3000);
