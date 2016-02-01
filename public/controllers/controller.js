cf6da602e9e9fcd32df02efe6ebd9a496b6fce7e
Yoonsungbin <freelifestyle2550@gmail.com>
2016-02-01 20:59:36 +0900

    Input MongoDB && output MongoDB

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,12 +7,14 @@ myApp.controller('AppCtrl',['$scope','$http',function($scope,$http) {
 	$http.get('/personlist').success(function (response) {
 		console.log('Got data from request');
 		$scope.personlist = response;
-	})
+	});
 
 	$scope.addPerson = function (){
 		console.log($scope.person);
-		$http.post('/personlist',$scope.person);
-	}
+		$http.post('/personlist1',$scope.person).success(function (response){
+			console.log(response);
+		});
+	};
 }]);
 
 
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express =require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('personlist',['personlist']);
+var bodyParser = require('body-parser');
+
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json()); 
 
 app.get('/personlist',function ( req , res ){
 	console.log("Get request from personlist");
@@ -11,6 +14,13 @@ app.get('/personlist',function ( req , res ){
  		console.log(docs);
  		res.json(docs);
  	});
+});
+
+app.post('/addperson', function (req, res) {
+	console.log(req.body);
+		db.personlist.insert(req.body, function (err, doc) {
+			res.json(doc);
+		});
 });
 
 app.listen(3000);
